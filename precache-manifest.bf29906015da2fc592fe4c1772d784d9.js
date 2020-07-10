07cfb079545b1052b468b96f704add97f420171c
vamc-stash <rangavamsi5@gmail.com>
2020-07-11 00:50:48 +0530

    Updates

---
+++ precache-manifest.bf29906015da2fc592fe4c1772d784d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acb74429db412a271930d99927f12bd1",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "f8662ddbb66188288527",
+    "url": "/calculator/static/css/main.452068ed.chunk.css"
+  },
+  {
+    "revision": "48926d9c80cce50b0776",
+    "url": "/calculator/static/js/2.2135fec4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.2135fec4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8662ddbb66188288527",
+    "url": "/calculator/static/js/main.62fa177b.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
